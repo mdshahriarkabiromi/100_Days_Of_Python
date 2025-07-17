9ff8498728636faf20a61532673b62bd04f2e946
mdshahriarkabiromi <shahriar.eu.cse59@gmail.com>
2025-07-17 20:52:19 +0600

    Added Day 1

---
+++ Day 1/1st code.py
@@ -0,0 +1,16 @@
+print("This is my first python code")
+print("Hello everyone")
+print("2+2") #everything that goes inside a inverted comma get printed as it is
+print(2+2) #integer so their result will be printed
+
+name=input("Enter your name:") #input function is used to get input from user
+print(f"Hello {name}, Good Morning!") #f string is used to print variable in a string
+name=("shahriar" +" "+"Kabir") # string concatenation, used to add two string together
+print(name)
+print(len(name))
+
+#rules of variable naming
+#Can not start with numbers
+#have to be more specific
+#cant be a constant
+#cant have whitespace
